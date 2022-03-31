40fb2f30b1857a719a755ac7012ec3927cce105d
shafucheng <flynn.sha@outlook.com>
2022-04-01 02:38:10 +0800

    feat: ReactDOM.render() 升级为 React 18 写法

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOMClient from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error('root not found')
+}
+
+const root = ReactDOMClient.createRoot(container)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 )
