9d08337a8a851d9606b9bf476403fe7ebec86bdd
shafucheng <flynn.sha@outlook.com>
2022-04-01 04:07:20 +0800

    chore: 使用 vite-plugin-html 将 env 注入到 html 中

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { defineConfig, UserConfig } from 'vite'
+import { defineConfig, loadEnv, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
   const common: UserConfig = {
-    plugins: [react(), eslint()],
+    plugins: [
+      react(),
+      eslint(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            ...env,
+          },
+        },
+      }),
+    ],
     resolve: {
       alias: {
         '~': path.resolve(__dirname, 'src'),
