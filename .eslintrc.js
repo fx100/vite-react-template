308f378f334d2ceb54aeb2689971518ad6b9bc45
shafucheng <flynn.sha@outlook.com>
2022-04-01 04:13:19 +0800

    chore: eslint-plugin-react 指定 react 版本 'detect'

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,4 +23,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
