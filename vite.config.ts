80bd1dcaee9d07b350859fe7b76838d5daa555e0
shafucheng <flynn.sha@outlook.com>
2022-04-01 03:41:49 +0800

    chore: @vitejs/plugin-legacy 使用 .browserslistrc 文件作为 targets 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
+import * as fs from 'fs'
 import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    legacy({
+      targets: fs
+        .readFileSync('.browserslistrc', 'utf-8')
+        .split('\n')
+        .filter((item) => item),
+    }),
+    eslint(),
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'),
