4079e32dde85827b31c3db74384e24eee756ad38
shafucheng <flynn.sha@outlook.com>
2022-04-03 05:10:55 +0800

    feat: dayjs 全局设置中文语言包

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOMClient from 'react-dom/client'
 import App from './App'
 
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs.locale('zh-cn')
+
 const container = document.getElementById('root')
 
 if (!container) {
