3b958fddb7ddcc11dfc1fddcfc33cb7a0ef9d5c2
shafucheng <flynn.sha@outlook.com>
2022-04-07 17:18:00 +0800

    feat(mock): 随机成功或失败模拟

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,12 +3,18 @@ import { rest } from 'msw'
 export const handlers = [
   rest.get('/api/user/:userId', (req, res, ctx) => {
     const { userId } = req.params
-    return res(
-      ctx.status(200),
-      ctx.json({
-        userId,
-        username: `username-${userId}`,
-      }),
-    )
+
+    if (Math.random() > 0.5) {
+      return res(
+        ctx.delay(1000),
+        ctx.status(200),
+        ctx.json({
+          userId,
+          username: `username-${userId}`,
+        }),
+      )
+    } else {
+      return res(ctx.delay(1000), ctx.status(500))
+    }
   }),
 ]
