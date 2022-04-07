38b433dccdaeb4954f328045dfbb7c8bb8b930a4
shafucheng <flynn.sha@outlook.com>
2022-04-07 22:26:15 +0800

    feat: 使用 antd-pro-layout 布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
+import Layout from '~/layouts/Layout'
 import SWRConfigPreset from '~/components/SWRConfigPreset'
 
 const lazyWithSuspense = (
@@ -22,37 +23,34 @@ const App = () => {
     <ConfigProvider locale={zhCN}>
       <SWRConfigPreset>
         <BrowserRouter>
-          <p>Hello Vite + React!</p>
-          <div style={{ display: 'flex', gap: '8px' }}>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-          </div>
           <Routes>
-            <Route
-              path="/"
-              element={lazyWithSuspense(() => import('~/pages/Home'))}
-            />
-            <Route
-              path="/about"
-              element={lazyWithSuspense(() => import('~/pages/About'))}
-            >
+            <Route path="/" element={<Layout />}>
               <Route
-                path="a"
-                element={lazyWithSuspense(() => import('~/pages/AboutA'))}
+                path="/"
+                element={lazyWithSuspense(() => import('~/pages/Home'))}
               />
               <Route
-                path="b"
-                element={lazyWithSuspense(() => import('~/pages/AboutB'))}
-              />
+                path="/about"
+                element={lazyWithSuspense(() => import('~/pages/About'))}
+              >
+                <Route
+                  path="a"
+                  element={lazyWithSuspense(() => import('~/pages/AboutA'))}
+                />
+                <Route
+                  path="b"
+                  element={lazyWithSuspense(() => import('~/pages/AboutB'))}
+                />
+                <Route
+                  path="*"
+                  element={lazyWithSuspense(() => import('~/pages/NotFound'))}
+                />
+              </Route>
               <Route
                 path="*"
                 element={lazyWithSuspense(() => import('~/pages/NotFound'))}
               />
             </Route>
-            <Route
-              path="*"
-              element={lazyWithSuspense(() => import('~/pages/NotFound'))}
-            />
           </Routes>
         </BrowserRouter>
       </SWRConfigPreset>
