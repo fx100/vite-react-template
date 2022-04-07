925abaf4251882e95796b987503ed519d52cae55
shafucheng <flynn.sha@outlook.com>
2022-04-07 17:18:53 +0800

    feat: 测试 mock API 请求

--- src/pages/AboutA.tsx
+++ src/pages/AboutA.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const AboutA = () => {
-  return <div>AboutA</div>
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState<Record<string, string>>({})
+
+  const fetchData = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get('/api/user/1')
+      setData(res.data)
+    } catch (err) {
+      setData({
+        error: (err as Error)?.message,
+      })
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <div>AboutA</div>
+      <button disabled={loading} onClick={fetchData}>
+        刷新
+      </button>
+      <div>状态：{loading ? '加载中' : data?.error ? '失败' : '成功'}</div>
+      <div>数据：{!loading && JSON.stringify(data)}</div>
+    </>
+  )
 }
 
 export default AboutA
